ff6be3cfc09f6103170b38503d44d3f32ae9379a
José Carlos Gil <josecgil@gmail.com>
2018-02-16 16:20:26 +0100

    Better first sample

--- Samples/sample.js
+++ Samples/sample.js
@@ -1,7 +1,18 @@
 var CountryStatesSelectorComponent=function(divComponent){
     var selectCountry=divComponent.querySelector("[data-country]");
     var selectState=divComponent.querySelector("[data-state]");
-    if (selectCountry.options[selectCountry.selectedIndex].value==="US") {
+
+    var selectedCountry = selectCountry.options[selectCountry.selectedIndex];
+    this._changeStateVisibility(selectedCountry, selectState);
+
+    var self=this;
+    selectCountry.onchange=function changeEventHandler(event) {
+        self._changeStateVisibility(event.target, selectState);
+    };
+};
+
+CountryStatesSelectorComponent.prototype._changeStateVisibility= function(selectedCountry, selectState){
+    if (selectedCountry.value==="US") {
         selectState.style.display="block";
     } else{
         selectState.style.display="none";
