7e12be6f521347d467bd9524d715e5931233cc61
José Carlos Gil <josecgil@gmail.com>
2018-04-14 20:00:00 +0200

    Added messages to bundle shell script

--- tools/bundle.sh
+++ tools/bundle.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/env bash
+echo
+echo "****************** INIT *******************"
 
-echo "concatenating ray files into a single file into /dist/ray.js..."
+echo "Step 1: Concatenating ray files into a single file into /dist/ray.js..."
 cat src/*.js >dist/ray.js
-echo "minifying ray file into /dist/ray-min.js..."
+
+echo "Step 2: Minifying ray file into /dist/ray-min.js..."
 uglifyjs dist/ray.js --compress --mangle --output dist/ray-min.js
-echo "done!"
+
+echo "****************** DONE *******************"
