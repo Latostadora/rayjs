cf0473bbb54d025b00f933f2d1dc3994e198a9d8
José Carlos Gil <josecgil@gmail.com>
2018-04-14 19:55:32 +0200

    Test to check double execution on load Removed commented test

--- dist/ray.js
+++ dist/ray.js
@@ -54,7 +54,7 @@
         }
         return false;
     };
-    
+
     exports.RayNS.Document=Document;
 })(window);
 
--- tools/bundle.sh
+++ tools/bundle.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
 
+echo "concatenating ray files into a single file into /dist/ray.js..."
 cat src/*.js >dist/ray.js
+echo "minifying ray file into /dist/ray-min.js..."
 uglifyjs dist/ray.js --compress --mangle --output dist/ray-min.js
+echo "done!"
