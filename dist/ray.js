3553e1b02522e6980095b47c8b6b415f334c26cf
José Carlos Gil <josecgil@gmail.com>
2018-04-22 19:46:21 +0200

    Simplified EventBus dependencies even more

--- dist/ray.js
+++ dist/ray.js
@@ -156,87 +156,65 @@ if (!Array.prototype.forEach) {
 })(window);
 //Adapted from https://gist.github.com/fatihacet/1290216
 
-var PubSub=function() {
-    this.init();
-};
-
-PubSub.prototype.init=function() {
-    this.topics = {};
-    this.id = 0;
-};
-
-PubSub.prototype.subscribe = function(topic, callback) {
-    if (!this.topics[topic]) {
-        this.topics[topic] = [];
-    }
-    this.id++;
-    this.topics[topic].push({
-        id: this.id,
-        callback: callback
-    });
-    return this.id;
-};
-
-PubSub.prototype.unsubscribe = function(id) {
-    for (var m in this.topics) {
-        if (!this.topics[m]) return false;
-        for (var i = 0, len = this.topics[m].length; i < len; i++) {
-            if (this.topics[m][i].id === id) {
-                this.topics[m].splice(i, 1);
-                return true;
-            }
-        }
-    }
-    return false;
-};
-
-PubSub.prototype.unsubscribeAll = function() {
-    this.init();
-};
-
-PubSub.prototype.publish = function(topic, args) {
-    if (!this.topics[topic]) {
-        return false;
-    }
-    var self=this;
-    setTimeout(function() {
-        var subscribers = self.topics[topic];
-        if (!subscribers) return;
-        subscribers.forEach(function(suscriber){
-           suscriber.callback(topic, args);
-        });
-    }, 0);
-    return true;
-};(function (exports) {
+(function (exports) {
 
     exports.RayNS=exports.RayNS || {};
 
-    var EventBus = function() {
-        this.pubSub=new PubSub();
+    var EventBus=function() {
+        this._init();
     };
 
-    EventBus.prototype.trigger = function (name, data) {
-        this.pubSub.publish(name, data);
+    EventBus.prototype._init=function() {
+        this.topics = {};
+        this.id = 0;
     };
 
-    EventBus.prototype.on = function (eventName, callback) {
-        return this.pubSub.subscribe(eventName, function (eventName, eventData ) {
-            callback(eventData);
-            return null;
+    EventBus.prototype.on = function(topic, callback) {
+        if (!this.topics[topic]) {
+            this.topics[topic] = [];
+        }
+        this.id++;
+        this.topics[topic].push({
+            id: this.id,
+            callback: callback
         });
+        return this.id;
     };
 
-    EventBus.prototype.off = function (id) {
-        this.pubSub.unsubscribe(id);
+    EventBus.prototype.off = function(id) {
+        for (var m in this.topics) {
+            if (!this.topics[m]) return false;
+            for (var i = 0, len = this.topics[m].length; i < len; i++) {
+                if (this.topics[m][i].id === id) {
+                    this.topics[m].splice(i, 1);
+                    return true;
+                }
+            }
+        }
+        return false;
     };
 
-    EventBus.prototype.end= function() {
-        this.pubSub.unsubscribeAll();
+    EventBus.prototype.end = function() {
+        this._init();
+    };
+
+    EventBus.prototype.trigger = function(topic, args) {
+        if (!this.topics[topic]) {
+            return false;
+        }
+        var self=this;
+        setTimeout(function() {
+            var subscribers = self.topics[topic];
+            if (!subscribers) return;
+            subscribers.forEach(function(suscriber){
+                suscriber.callback(args);
+            });
+        }, 0);
+        return true;
     };
 
     exports.RayNS.EventBus=EventBus;
 })(window);
-
 
 
 (function (exports) {
--- src/30_event-bus.js
+++ src/30_event-bus.js
@@ -1,32 +1,63 @@
+//Adapted from https://gist.github.com/fatihacet/1290216
+
 (function (exports) {
 
     exports.RayNS=exports.RayNS || {};
 
-    var EventBus = function() {
-        this.pubSub=new PubSub();
+    var EventBus=function() {
+        this._init();
     };
 
-    EventBus.prototype.trigger = function (name, data) {
-        this.pubSub.publish(name, data);
+    EventBus.prototype._init=function() {
+        this.topics = {};
+        this.id = 0;
     };
 
-    EventBus.prototype.on = function (eventName, callback) {
-        return this.pubSub.subscribe(eventName, function (eventName, eventData ) {
-            callback(eventData);
-            return null;
+    EventBus.prototype.on = function(topic, callback) {
+        if (!this.topics[topic]) {
+            this.topics[topic] = [];
+        }
+        this.id++;
+        this.topics[topic].push({
+            id: this.id,
+            callback: callback
         });
+        return this.id;
     };
 
-    EventBus.prototype.off = function (id) {
-        this.pubSub.unsubscribe(id);
+    EventBus.prototype.off = function(id) {
+        for (var m in this.topics) {
+            if (!this.topics[m]) return false;
+            for (var i = 0, len = this.topics[m].length; i < len; i++) {
+                if (this.topics[m][i].id === id) {
+                    this.topics[m].splice(i, 1);
+                    return true;
+                }
+            }
+        }
+        return false;
     };
 
-    EventBus.prototype.end= function() {
-        this.pubSub.unsubscribeAll();
+    EventBus.prototype.end = function() {
+        this._init();
+    };
+
+    EventBus.prototype.trigger = function(topic, args) {
+        if (!this.topics[topic]) {
+            return false;
+        }
+        var self=this;
+        setTimeout(function() {
+            var subscribers = self.topics[topic];
+            if (!subscribers) return;
+            subscribers.forEach(function(suscriber){
+                suscriber.callback(args);
+            });
+        }, 0);
+        return true;
     };
 
     exports.RayNS.EventBus=EventBus;
 })(window);
-
 
 
