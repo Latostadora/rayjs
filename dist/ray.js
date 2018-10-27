bd09db0df128ab4734b46afb58709c69ce846519
José Carlos Gil <josecgil@gmail.com>
2018-10-27 22:48:21 +0200

    All js migrated to ES6 reorganized directories && build system

--- dist/ray.js
+++ dist/ray.js
@@ -345,46 +345,60 @@ var Bus = function () {
 }();
 
 window.RayNS = window.RayNS || {};
-window.RayNS.Bus = Bus;(function (exports) {
+window.RayNS.Bus = Bus;'use strict';
 
-    exports.RayNS=exports.RayNS || {};
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
-    var Ray=function(eventNamesToListen) {
-        this.eventNamesToListen=eventNamesToListen || {document:'DOMContentLoaded', window:'load'};
-        this.raydocument=new RayNS.Document(this.eventNamesToListen);
-        this.eventBus=RayNS.Bus.create();
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+var Ray = function () {
+    function Ray(eventNamesToListen) {
+        _classCallCheck(this, Ray);
+
+        this.eventNamesToListen = eventNamesToListen || { document: 'DOMContentLoaded', window: 'load' };
+        this.raydocument = new RayNS.Document(this.eventNamesToListen);
+        this.eventBus = RayNS.Bus.create();
         this.commandDispatcher = new RayNS.CommandDispatcher(this.eventBus);
-    };
+    }
 
-    Ray.prototype.begin=function() {
-        this.raydocument.begin();
-        var self = this;
-        this.raydocument.ready(function(){
-            self.commandDispatcher.loadNewComponents();
-        });
-        this.intervalId=setInterval(function(){
-            self.commandDispatcher.loadNewComponents();
-        },400);
-    };
+    _createClass(Ray, [{
+        key: 'begin',
+        value: function begin() {
+            this.raydocument.begin();
+            var self = this;
+            this.raydocument.ready(function () {
+                self.commandDispatcher.loadNewComponents();
+            });
+            this.intervalId = setInterval(function () {
+                self.commandDispatcher.loadNewComponents();
+            }, 400);
+        }
+    }, {
+        key: 'end',
+        value: function end() {
+            clearInterval(this.intervalId);
+            this.raydocument.end();
+            this.eventBus.end();
+        }
+    }, {
+        key: 'getCommandDispatcher',
+        value: function getCommandDispatcher() {
+            return this.commandDispatcher;
+        }
+    }], [{
+        key: 'createBus',
+        value: function createBus() {
+            return RayNS.Bus.create();
+        }
+    }, {
+        key: 'createComponent',
+        value: function createComponent(domElement, bus) {
+            return RayNS.Component.create(domElement, bus);
+        }
+    }]);
 
-    Ray.prototype.end=function() {
-        clearInterval(this.intervalId);
-        this.raydocument.end();
-        this.eventBus.end();
-    };
+    return Ray;
+}();
 
-    Ray.prototype.getCommandDispatcher=function() {
-        return this.commandDispatcher;
-    };
-
-    Ray.createBus=function() {
-        return RayNS.Bus.create();
-    };
-
-    Ray.createComponent=function(domElement, bus) {
-        return RayNS.Component.create(domElement, bus);
-    };
-
-    exports.RayNS.Ray=Ray;
-})(window);
-
+window.RayNS = window.RayNS || {};
+window.RayNS.Ray = Ray;
--- src/20-command-dispatcher.js
+++ src/20-command-dispatcher.js
@@ -1,43 +1,33 @@
-'use strict';
 
-var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+const Component=RayNS.Component;
 
-function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
-
-var Component = RayNS.Component;
-
-var CommandDispatcher = function () {
-    function CommandDispatcher(eventBus) {
-        _classCallCheck(this, CommandDispatcher);
-
+class CommandDispatcher {
+    constructor(eventBus) {
         this.eventBus = eventBus;
     }
 
-    _createClass(CommandDispatcher, [{
-        key: 'loadNewComponents',
-        value: function loadNewComponents() {
-            var DATA_RAY_ATTR = RayNS.Component.DATA_RAY_ATTR;
-            var self = this;
-            return document.querySelectorAll('[' + DATA_RAY_ATTR + ']').forEach(function (domElement) {
-                try {
-                    var EXECUTED_ATTRIBUTE = 'data-ray-component-executed';
-                    if (domElement.hasAttribute(EXECUTED_ATTRIBUTE)) {
-                        return;
-                    }
-                    domElement.setAttribute(EXECUTED_ATTRIBUTE, '');
-
-                    var component = Component.create(domElement, self.eventBus);
-                    component.execute();
-                } catch (e) {
-                    self.eventBus.trigger("ray.error", e);
-                    console.log('RayJS: Error loading components: ' + e.message);
+    loadNewComponents() {
+        const DATA_RAY_ATTR=RayNS.Component.DATA_RAY_ATTR;
+        const self = this;
+        return document.querySelectorAll(`[${DATA_RAY_ATTR}]`).forEach(domElement => {
+            try {
+                const EXECUTED_ATTRIBUTE = 'data-ray-component-executed';
+                if (domElement.hasAttribute(EXECUTED_ATTRIBUTE)) {
+                    return;
                 }
-            });
-        }
-    }]);
+                domElement.setAttribute(EXECUTED_ATTRIBUTE, '');
 
-    return CommandDispatcher;
-}();
+                const component=Component.create(domElement, self.eventBus);
+                component.execute();
+            } catch (e) {
+                self.eventBus.trigger("ray.error", e);
+                console.log(`RayJS: Error loading components: ${e.message}`);
+            }
+        });
+    }
+}
 
-window.RayNS = window.RayNS || {};
-window.RayNS.CommandDispatcher = CommandDispatcher;
+window.RayNS=window.RayNS || {};
+window.RayNS.CommandDispatcher=CommandDispatcher;
+
+
