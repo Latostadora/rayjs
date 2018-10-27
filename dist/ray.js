9870c25157fb1ea7f6ac77bb138b6b3232bb6743
José Carlos Gil <josecgil@gmail.com>
2018-10-27 21:22:07 +0200

    Migrated component to ES6 Added command npm run build Modified command nom test to build before test

--- dist/ray.js
+++ dist/ray.js
@@ -160,55 +160,68 @@ var ComponentData = function () {
 }();
 
 window.RayNS = window.RayNS || {};
-window.RayNS.ComponentData = ComponentData;(function (exports) {
+window.RayNS.ComponentData = ComponentData;"use strict";
 
-    exports.RayNS=exports.RayNS || {};
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
-    var Component=function(componentConstructorFn, data) {
-        this.componentConstructorFn=componentConstructorFn;
-        this.data=data;
-    };
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
-    Component.prototype.execute=function(){
-        return new this.componentConstructorFn(this.data);
-    };
+var Component = function () {
+    function Component(componentConstructorFn, data) {
+        _classCallCheck(this, Component);
 
-    Component.DATA_RAY_ATTR= "data-ray-component";
+        this.componentConstructorFn = componentConstructorFn;
+        this.data = data;
+    }
 
-    Component.create=function(domElement, bus) {
-        function getComponentName(dataRayComponentAttrValue) {
-            var namespaces = dataRayComponentAttrValue.split(".");
-            return namespaces.pop();
+    _createClass(Component, [{
+        key: "execute",
+        value: function execute() {
+            return new this.componentConstructorFn(this.data);
         }
+    }], [{
+        key: "create",
+        value: function create(domElement, bus) {
 
-        function getLastNamespaceObject(dataRayComponentAttrValue) {
-            var namespaces = dataRayComponentAttrValue.split(".");
-            namespaces.pop();
+            var getComponentName = function getComponentName(dataRayComponentAttrValue) {
+                var namespaces = dataRayComponentAttrValue.split(".");
+                return namespaces.pop();
+            };
 
-            var obj=window;
-            namespaces.forEach(function(namespace){
-                obj=obj[namespace];
-            });
-            return obj;
+            var getLastNamespaceObject = function getLastNamespaceObject(dataRayComponentAttrValue) {
+                var namespaces = dataRayComponentAttrValue.split(".");
+                namespaces.pop();
+
+                var obj = window;
+                namespaces.forEach(function (namespace) {
+                    obj = obj[namespace];
+                });
+                return obj;
+            };
+
+            var data = RayNS.ComponentData.create(domElement, bus);
+
+            var dataRayComponentAttrValue = domElement.getAttribute(Component.DATA_RAY_ATTR);
+            var componentName = getComponentName(dataRayComponentAttrValue);
+            var lastNamespaceObject = getLastNamespaceObject(dataRayComponentAttrValue);
+            var componentConstructorFn = lastNamespaceObject[componentName];
+            if (componentConstructorFn === undefined) {
+                throw new Error("<" + componentName + "> JS object not Found");
+            }
+            return new Component(componentConstructorFn, data);
         }
-
-        var data=RayNS.ComponentData.create(domElement, bus);
-
-        var dataRayComponentAttrValue = domElement.getAttribute(Component.DATA_RAY_ATTR);
-        var componentName = getComponentName(dataRayComponentAttrValue);
-        var lastNamespaceObject = getLastNamespaceObject(dataRayComponentAttrValue);
-        var componentConstructorFn = lastNamespaceObject[componentName];
-        if (componentConstructorFn==undefined) {
-            throw new Error("<"+componentName+"> JS object not Found");
+    }, {
+        key: "DATA_RAY_ATTR",
+        get: function get() {
+            return "data-ray-component";
         }
-        return new Component(componentConstructorFn, data);
-    };
+    }]);
 
-    exports.RayNS.Component=Component;
-})(window);
+    return Component;
+}();
 
-
-(function (exports) {
+window.RayNS = window.RayNS || {};
+window.RayNS.Component = Component;(function (exports) {
 
     exports.RayNS=exports.RayNS || {};
 
